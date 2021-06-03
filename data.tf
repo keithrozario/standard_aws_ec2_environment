742cf5e0b01b4cbc74314569e8034866d2210aaf
Keith Rozario <krozario@amazon.com>
2021-06-03 10:55:12 +0800

    new data

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
-data "aws_security_group" "workspacesSG" {
-  id = "sg-0636c17643e41b430"
-}
+# data "aws_security_group" "workspacesSG" {
+#   id = "sg-0636c17643e41b430"
+# }
 
 locals {
   common_tags = {
